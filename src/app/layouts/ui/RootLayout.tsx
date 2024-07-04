fa621094d2df441976abf63a40d006c286e756ba
kasmil <kamilsalikhov75@gmail.com>
2024-07-04 13:27:13 +0300

    fix main height

--- src/app/layouts/ui/RootLayout.tsx
+++ src/app/layouts/ui/RootLayout.tsx
@@ -9,7 +9,7 @@ export const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <>
       <Header />
-      <main className="h-[calc(100vh-165px)] overflow-y-scroll px-3 overflow-x-hidden">
+      <main className="h-[calc(100dvh-165px)] overflow-y-scroll px-3 overflow-x-hidden">
         {children}
       </main>
       <Footer />
